bfc911f71a73acdd6756f6e832e19f9f55eed603
lyprince <lukep@graphcore.ai>
2023-03-03 13:20:34 +0000

    refactor collective tests

---
+++ tests/test_collectives.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2023 Graphcore Ltd. All rights reserved.
+
+import random
+from copy import deepcopy
+from typing import Any, Callable, Tuple
+
+import einops
+import numpy as np
+import poptorch
+import pytest
+import torch
+import torch.nn as nn
+from poptorch.enums import CommGroupType
+
+import poptorch_experimental_addons as pea
+
+from .utils import _apply_replica_grouping
+
+assert_close = torch.testing.assert_close  # type:ignore[attr-defined]
+
+
+def set_seed(seed: int) -> None:
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
+class _CollectiveCrossReplicaTester(torch.nn.Module):
+    def __init__(
+        self,
+        X: torch.Tensor,
+        replication_factor: int,
+        cross_replica_op: Callable,
+    ):
+        super().__init__()
+        self.X = nn.Parameter(
+            einops.rearrange(X, "(r n) d -> r n d", r=replication_factor).contiguous()
+        )
+        self.replication_factor = replication_factor
+        self.cross_replica_op = cross_replica_op
+
+    def forward(self) -> Tuple[Any, Any]:
+        out = self.cross_replica_op(self.X, self.replication_factor)
+        return out, poptorch.identity_loss(out.pow(2), reduction="sum")
+
+
+class _CollectiveSimulator(torch.nn.Module):
+    def __init__(
+        self, X: torch.Tensor, replication_factor: int, simulator_op: Callable
+    ):
+        super().__init__()
+        self.X = nn.Parameter(
+            einops.rearrange(X, "(r n) d -> r n d", r=replication_factor).contiguous()
+        )
+        self.replication_factor = replication_factor
+        self.simulator_op = simulator_op
+
+    def forward(self) -> Tuple[Any, Any]:
+        out = self.simulator_op(self.X, self.replication_factor)
+        loss = out.pow(2).sum(dim=list(range(out.ndim))[1:])
+        return torch.vstack([*out]), loss
+
+
+def _all_gather_simulator_op(X: torch.Tensor, replication_factor: int) -> torch.Tensor:
+    return einops.repeat(X, "s n d -> r s n d", r=replication_factor)
+
+
+def _all_reduce_simulator_op(X: torch.Tensor, replication_factor: int) -> torch.Tensor:
+    out = einops.reduce(X, "r n d -> n d", "sum")
+    return einops.repeat(out, "n d -> r n d", r=replication_factor)
+
+
+_op_mapping = {
+    pea.collectives.all_gather_cross_replica_mean_grad: _all_gather_simulator_op,
+    pea.collectives.all_reduce_cross_replica_sum: _all_reduce_simulator_op,
+}
+
+
+def generate_input() -> torch.Tensor:
+    return einops.rearrange(torch.arange(32, dtype=torch.float32), "(d n)  -> n d", n=4)
+
+
+def simulate_collective(
+    X: torch.Tensor, op: Callable, num_ipus: int
+) -> Tuple[Any, Any]:
+    sim = _CollectiveSimulator(
+        deepcopy(X), replication_factor=num_ipus, simulator_op=op
+    )
+    out, loss = sim()
+    loss.mean().backward()
+    grad = einops.rearrange(sim.X.grad, "r n d -> (r n) d")
+    return out, grad
+
+
+def run_collective(X: torch.Tensor, op: Callable, num_ipus: int) -> Tuple[Any, Any]:
+    options = poptorch.Options()
+    options.replicationFactor(num_ipus)
+    options.outputMode(poptorch.OutputMode.All)
+    options.useIpuModel(True)
+    options.anchorTensor("grad_X", "Gradient___X")
+    options._Popart.setPatterns({"OpToIdentity": True})
+
+    col = _CollectiveCrossReplicaTester(
+        X, replication_factor=num_ipus, cross_replica_op=op
+    )
+    optimizer = poptorch.optim.SGD(col.parameters(), lr=1.0)
+    col = poptorch.trainingModel(col, options, optimizer)
+    _apply_replica_grouping(col, CommGroupType.Orthogonal, 1)
+    out, _ = col()
+    out = out.detach().cpu()
+    grad = col.getAnchoredTensor("grad_X")  # type: ignore
+    return out, grad
+
+
+@pytest.mark.parametrize("op", list(_op_mapping.keys()))
+def test_collective(op: Callable) -> None:
+    X = generate_input()
+    num_ipus = 2
+    actual = run_collective(X, op, num_ipus)
+    expected = simulate_collective(X, _op_mapping[op], num_ipus)
+    map(assert_close, actual, expected)
